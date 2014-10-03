89314b013b3050bcc55e8a29208db9b39f93f41f
Iker Ibarguren <ikerib@gmail.com>
2014-10-03 13:36:51 +0200

    Raiz login2

--- src/Gitek/UserBundle/Entity/User.php
+++ src/Gitek/UserBundle/Entity/User.php
@@ -4,6 +4,7 @@ namespace Gitek\UserBundle\Entity;
 
 use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity
@@ -18,9 +19,22 @@ class User extends BaseUser
      */
     protected $id;
 
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank(message="Por favor escribe nombre.", groups={"Registration", "Profile"})
+     */
+    protected $nombre;
+
+    /**
+     * @Assert\File(maxSize="2048k")
+     * @Assert\Image(mimeTypesMessage="Please upload a valid image.")
+     */
+    protected $profilePictureFile;
+
     public function __construct()
     {
         parent::__construct();
         // your own logic
     }
+
 }

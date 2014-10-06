4486fff5f4adb754b2d3130165d4830ca8bb8816
Iker Ibarguren <ikerib@gmail.com>
2014-10-06 12:19:11 +0200

    Kendu fosuserbundle

--- src/Gitek/UserBundle/Entity/User.php
+++ src/Gitek/UserBundle/Entity/User.php
@@ -20,12 +20,6 @@ class User extends BaseUser
     protected $id;
 
     /**
-     * @ORM\Column(type="string", length=255)
-     * @Assert\NotBlank(message="Por favor escribe nombre.", groups={"Registration", "Profile"})
-     */
-    protected $nombre;
-
-    /**
      * @Assert\File(maxSize="2048k")
      * @Assert\Image(mimeTypesMessage="Please upload a valid image.")
      */

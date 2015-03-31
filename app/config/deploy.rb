a7cd140363ff74a73dad8d20321ee75b6980b5e1
Iker Ibarguren <ikerib@gmail.com>
2015-03-31 10:56:00 +0200

    Deploy.rb

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -21,11 +21,6 @@ role :web,        domain                         # Your HTTP server, Apache/etc
 role :app,        domain, :primary => true       # This may be the same as your `Web` server
 #role :db,         domain, :primary => true       # This is where Rails migrations will run
 
-set :writable_dirs,       ["app/cache", "app/logs"]
-set :webserver_user,      "www-data"
-set :permission_method,   :acl
-set :use_set_permissions, true
-
 set  :use_sudo,         false
 set  :keep_releases,    5
 set  :shared_files,     ["app/config/parameters.yml"]
